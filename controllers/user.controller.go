33b64c6a83fb6b8fd6ca679625ad917b743ed7ff
Abhinav <AbhinavSunil@hotmail.com>
2026-07-10 11:09:48 +0530

    Feat : Added user succesfully to database ,  NOW GOING TO work on foramtting and marshalling

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -18,24 +18,8 @@ func NewController(_user_service services.UserService) *UserController{
 
 func (controller *UserController) SignUp(w http.ResponseWriter, r *http.Request) {
 
-	var user struct {
-		Name     string `json:"name"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
 
-	err := r.ParseForm()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Error parsing form data"))
-		return
-	}
-
-	user.Name = r.FormValue("name")
-	user.Email = r.FormValue("email")
-	user.Password = r.FormValue("password")
-
-	response, err := controller.UserService.SignUp(user.Name, user.Email, user.Password)
+	response, err := controller.UserService.SignUp("Abhinav", "abhinavsunil70@gmail.com", "abhinavs784d")
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
