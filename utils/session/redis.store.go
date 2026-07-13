735c57a24bd50fce40f2fe14860abcecd1a9227f
Abhinav <AbhinavSunil@hotmail.com>
2026-07-13 10:54:06 +0530

    Feat : Implementing redis methods , implemeted save and get method , next moving forward with integrating with userSignUp

--- utils/session/redis.store.go
+++ utils/session/redis.store.go
@@ -1,16 +1,18 @@
 package session
 
 import (
+	"Coderx/utils/json"
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
 type SessionStore interface {
-	save(ctx context.Context,sessionId string,data map[string]string,expiry time.Duration) error
-	get(ctx context.Context,sessionId string) (map[string]string,error)
-	destroy(ctx context.Context,sessionId string) error
+	Save(ctx context.Context,sessionId string,data map[string]string,expiry time.Duration) error
+	Get(ctx context.Context,sessionId string) (map[string]string,error)
+	Destroy(ctx context.Context,sessionId string) error
 }
 
 type SessionManager struct{
@@ -25,9 +27,28 @@ func NewSessionStore(redis_client *redis.Client) *SessionManager{
 
 func( store *SessionManager) save(ctx context.Context,sessionId string,data map[string]string,expiry time.Duration) error{
 
+	bytes,err := json.Marshall(data)
 
-	
+	if err != nil{
+		fmt.Println("Error Happend while Marshalling data")
+		return err
+	}
 
+	return store.redis_client.Set(ctx,"session:"+sessionId,bytes,expiry).Err()
 
+}
+
+func (store * SessionManager) Get(ctx context.Context,sessionId string) (map[string]string,error){
+
+	bytes,err := store.redis_client.Get(ctx,"session:"+sessionId).Bytes()
+
+	if err != nil{
+		fmt.Println("Error occured while retrieved data from redis")
+		return nil,err
+	}
+
+	var data map[string]string
+	err = json.UnMarshall(bytes,&data)
+	return data,err
 
 }
